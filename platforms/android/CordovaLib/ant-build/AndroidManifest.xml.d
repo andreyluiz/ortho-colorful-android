c191336a1a63ebf617b68b2065d47438c4683914
Andrey Luiz <andreyluiz.developer@gmail.com>
2015-01-05 21:58:50 -0200

    Ortho colorful cordova android project

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Andrey\Git\ortho-colorful-android\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
+C:\Users\Andrey\Git\ortho-colorful-android\platforms\android\CordovaLib\AndroidManifest.xml \
---
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -0,0 +1,1 @@
+ : C:\Users\Andrey\Git\ortho-colorful-android\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
